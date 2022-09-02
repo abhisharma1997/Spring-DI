cac2b3b4109a8041fbe77f82d354f3f419731676
Abhishek <sharma.abhishek1739@gmail.com>
2022-09-02 11:02:29 +0530

    Dependency Injection impl for Collection Map of Objects

--- src/main/java/com/learning/spring/di/pojo/Technologies.java
+++ src/main/java/com/learning/spring/di/pojo/Technologies.java
@@ -1,13 +1,18 @@
 package com.learning.spring.di.pojo;
 
+import java.util.Map;
+
 public class Technologies {
     private String name;
+
+    private Map<String,String> exp;
 
     public Technologies() {
     }
 
-    public Technologies(String name) {
+    public Technologies(String name, Map<String,String> exp) {
         this.name = name;
+        this.exp=exp;
     }
 
     public String getName() {
@@ -19,10 +24,19 @@ public class Technologies {
         this.name = name;
     }
 
+    public Map<String, String> getExp() {
+        return exp;
+    }
+
+    public void setExp(Map<String, String> exp) {
+        this.exp = exp;
+    }
+
     @Override
     public String toString() {
         return "Technologies{" +
                 "name='" + name + '\'' +
+                ", exp=" + exp +
                 '}';
     }
 }
